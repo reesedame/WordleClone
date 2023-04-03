3f5d097ea16160569b514b52aaeb5e72ae1d361a
Reese Dame <reesedame@gmail.com>
2023-04-03 15:41:51 -0700

    Complete basic keyboard functionality

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
 /*----- constants -----*/
 
+const words = ["CHUNK", "CLOCK", "ADOPT", "DOZEN", "LAYER", "LEMON", "MEDAL"];
+const winningMsg = "Congrats! You identified the wordle!";
+
 /*----- state variables -----*/
+
+let numGuesses;
+let guesses;
+let wordle;
 
 /*----- cached elements  -----*/
 
+const gameMsg = document.getElementById("game-msg");
+
 /*----- event listeners -----*/
 
+document
+	.getElementById("keyboard")
+	.addEventListener("click", handleKeyboardClick);
+
 /*----- functions -----*/
+
+initialize();
+
+function initialize() {
+	numGuesses = 0;
+	guesses = [];
+	wordle = getRandomWord(words);
+	currentGuess = "";
+	currentGuessIdx = currentGuess.length - 1;
+}
+
+function getRandomWord(words) {
+	let randomIndex = Math.floor(Math.random() * words.length);
+	return words[randomIndex];
+}
+
+function handleKeyboardClick(e) {
+	const key = e.target.innerText;
+	if (key === "ENTER") {
+		handleEnter();
+	} else if (key === "") {
+		handleDelete();
+	} else {
+		currentGuess += key;
+		currentGuessIdx = currentGuess.length - 1;
+		document.getElementById(
+			`guess-${numGuesses}-idx-${currentGuessIdx}`
+		).innerText = key;
+	}
+}
